9f7793f2030c03e2e6050fe9b606e7de57ad4a05
Qad Noir <qadzealous@gmail.com>
2026-08-16 14:55:29 +0100

    Pass invoice ID to update Server Action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,26 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
+
+export async function updateInvoice(id: string, formData: FormData){
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    set customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    where id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
 
 export async function createInvoice(formData: FormData){
     const {customerId, amount, status} = CreateInvoice.parse({
